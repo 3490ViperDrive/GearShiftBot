4b9bb010bb04f393399a4c1cd4d64b52245596a6
kjr-code <kjr-code@gmail.com>
2023-09-19 12:35:45 -0400

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.DrivetrainConstants.*;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase{
+
+    //Drivetrain will need *motor controllers* at the very least. 
+    //A gyro may also be advisable for any sort of software-assisted navigation.
+
+
+
+    @Override
+    public void periodic(){
+
+    }
+}
