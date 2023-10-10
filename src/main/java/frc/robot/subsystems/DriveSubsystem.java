fd309bb3195f090518b82dbd37bcce435a25d48c
Jabari-R <jabarirheubottom@gmail.com>
2023-10-10 18:46:32 -0400

    Group Work 10/10 got em

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,12 +1,43 @@
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.DrivetrainConstants.*;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class DriveSubsystem extends SubsystemBase{
 
     //Drivetrain will need *motor controllers* at the very least. 
     //A gyro may also be advisable for any sort of software-assisted navigation.
+
+    private PWMSparkMax leftMotor1 = new PWMSparkMax(0);
+    private PWMSparkMax leftMotor2 = new PWMSparkMax(1);
+    private PWMSparkMax rightMotor1 = new PWMSparkMax(2);
+    private PWMSparkMax rightMotor2 = new PWMSparkMax(3);
+
+    private MotorControllerGroup leftMotors;
+    private MotorControllerGroup rightMotors;
+
+    public static void tankDrive(double leftSpeed, double rightSpeed){
+
+
+        leftMotors = new MotorControllerGroup(leftMotor1, leftMotor2, null);
+        rightMotors = new MotorControllerGroup(rightMotor1, rightMotor2, null);
+
+    }
+
+    
+
+
+    
+
+
+
+    
 
 
 
---
+++ src/main/java/frc/robot/subsystems/FlexSystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FlexSystem extends SubsystemBase{
+    //declare led objects 
+
+    public static void strobeTheLights(){
+        //strobe the lights
+    }
+}
